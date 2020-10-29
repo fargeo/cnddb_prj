97896eefde9e8d696fa7ce05719e7d221de29835
Adam Lodge <alodge@fargeo.com>
2020-10-29 13:33:34 -0700

    improvements to models

---
+++ cnddb/pkg/post_sql/create_observations_layer.sql
@@ -0,0 +1,5 @@
+INSERT INTO map_sources(name, source)
+    VALUES ('observations', '{
+        "data": "/geojson?nodeids=df79c02a-033f-11eb-9978-02e99e44e93e&include_geojson_link=true",
+        "type": "geojson"
+    }');
