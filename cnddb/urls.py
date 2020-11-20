9ff863ea6efb3b67028f26f78fb68e53852fcdf1
njkim <nkim@fargeo.com>
2020-11-20 13:22:40 -0800

    Add custom view for overlay map layers

--- cnddb/urls.py
+++ cnddb/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from cnddb.views.overlays import MVT_County, MVT_Quad
 
 urlpatterns = [
     url(r'^', include('arches.urls')),
+    url(
+        r"^mvt-county/(?P<zoom>[0-9]+|\{z\})/(?P<x>[0-9]+|\{x\})/(?P<y>[0-9]+|\{y\}).pbf$",
+        MVT_County.as_view(), name="mvt-conuty",
+    ),
+    url(
+        r"^mvt-quad/(?P<zoom>[0-9]+|\{z\})/(?P<x>[0-9]+|\{x\})/(?P<y>[0-9]+|\{y\}).pbf$",
+        MVT_Quad.as_view(), name="mvt-quad",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.SHOW_LANGUAGE_SWITCH is True:
