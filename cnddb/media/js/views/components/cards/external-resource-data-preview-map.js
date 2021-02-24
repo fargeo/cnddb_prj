68a2c20eea91c9dde3a47f0f71f79fc08267340a
Christopher Byrd <cbyrd@fargeo.com>
2021-02-23 19:35:53 -0800

    move all preview logic to cnddb #30

--- cnddb/media/js/views/components/cards/external-resource-data-preview-map.js
+++ cnddb/media/js/views/components/cards/external-resource-data-preview-map.js
@@ -2,54 +2,67 @@ define([
     'jquery',
     'arches',
     'knockout',
+    'knockout-mapping',
     'mapbox-gl',
+    'geojson-extent',
+    'viewmodels/card-component',
+    'viewmodels/map-editor',
+    'viewmodels/map-filter',
+    'views/components/cards/related-resources-map',
+    'views/components/cards/select-related-feature-layers',
+    'text!templates/views/components/cards/external-resource-data-preview-popup.htm',
     'views/components/external-resource-data-preview/external-resource-data-preview',
-    'text!templates/views/components/cards/external-resource-data-preview-popup.htm'
-], function($, arches, ko, mapboxgl, ExternalResourceDataPreview, popupTemplate) {
+
+], function($, arches, ko, koMapping, mapboxgl, geojsonExtent, CardComponentViewModel, MapEditorViewModel, MapFilterViewModel, RelatedResourcesMapCard, selectFeatureLayersFactory, popupTemplate) {
     /* 
         Used to connect the external-resource-data-preview component 
         with the related-resources-map-card
     */ 
     var viewModel = function(params) {
         var self = this;
+        ko.utils.extend(self, new RelatedResourcesMapCard(params));
 
-        params.popupTemplate = popupTemplate;
-        self.popupTemplate = popupTemplate;
+        this.draw = params.draw;
+        this.map = ko.observable(params.map);
 
-        this.map = params.map;
         this.fileData = ko.observable();
-
         this.fileData.subscribe(function(fileData) {
-            if (self.draw) {
-                self.updateMap(fileData)
+            if (!self.draw) {
+                self.draw = params.draw;
             }
+            if (!ko.unwrap(self.map)) {
+                self.map(params.map);
+            }
+            self.updateMap(fileData)
         });
 
         this.updateMap = function(fileData) {
-            self.draw.deleteAll();
+            if (self.draw) {
+                self.draw.deleteAll();
 
-            var bounds = new mapboxgl.LngLatBounds();
-    
-            fileData.forEach(function(fileDatum) {
-                fileDatum.data.forEach(function(parsedRow) {
-                    if (parsedRow.location_data) {
-                        parsedRow.location_data.features.forEach(function(feature) {
-                            feature.id = parsedRow.row_id;
-                            self.draw.add(feature);
-                            bounds.extend(feature.geometry.coordinates);
-                        });
-                    }
+                var bounds = new mapboxgl.LngLatBounds();
+        
+                fileData.forEach(function(fileDatum) {
+                    fileDatum.data.forEach(function(parsedRow) {
+                        if (parsedRow.location_data) {
+                            parsedRow.location_data.features.forEach(function(feature) {
+                                feature.id = parsedRow.row_id;
+                                self.draw.add(feature);
+                                bounds.extend(feature.geometry.coordinates);
+                            });
+                        }
+                    });
                 });
-            });
-
-            if (fileData.length) {
-                self.map().fitBounds(
-                    bounds, 
-                    { 
-                        padding: { top: 120, right: 540, bottom: 120, left: 120 },
-                        linear: true,
-                    }
-                );
+    
+                if (fileData.length) {
+                    self.map().fitBounds(
+                        bounds, 
+                        { 
+                            padding: { top: 120, right: 540, bottom: 120, left: 120 },
+                            linear: true,
+                        }
+                    );
+                }
             }
         }
 
@@ -67,7 +80,7 @@ define([
                     map.queryRenderedFeatures(e.point),
                     function(feature) { return feature.properties.id; }
                 );
-                
+
                 if (hoverFeature) {
                     hoverFeature.id = hoverFeature.properties.id;
 
@@ -82,7 +95,7 @@ define([
                     if (featureData) {
                         self.popup = new mapboxgl.Popup()
                             .setLngLat(e.lngLat)
-                            .setHTML(self.popupTemplate)
+                            .setHTML(popupTemplate)
                             .addTo(map);
                         ko.applyBindingsToDescendants(
                             featureData,
@@ -98,7 +111,6 @@ define([
                             self.popup = undefined;
                         });
                     }
-
                 }
             });
         });
@@ -107,7 +119,7 @@ define([
     ko.components.register('external-resource-data-preview-map', {
         viewModel: viewModel,
         template: {
-            require: 'text!templates/views/components/cards/related-resources-map.htm'
+            require: 'text!templates/views/components/cards/external-resource-data-preview-map.htm'
         }
     });
 
